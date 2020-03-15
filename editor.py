71cc61852cf966df38815b02167df991a5f13037
Scott <k1206150@students.katyisd.org>
2020-03-15 11:29:53 -0500

    ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- editor.py
+++ editor.py
@@ -1,16 +1,24 @@
 class editor:
     def __init__(self, filename=""):
         self.fd = filename
+        self.cursor = 0
         if  self.fd!="" :  # Not ""
-            self.source = self.read(filename)
+            self.source = self.read(filename,caller=1)
         else:
             self.source = []
-    def read(self, fn):
-        return open(fn, 'r').read().split('\n')
+    def read(self, fn, caller=0):
+        try:
+            return open(fn, 'r').read().split('\n')
+        except FileNotFoundError:
+            if caller == 1:
+                print("No such file or directory")
+            else:
+                return 1
     def append(self, arr):
-        assert type(arr) is list
         for item in arr:
-            self.source.append(item)
+            self.source.insert(self.cursor, item)
+            self.cursor +=1
+
     def write(self):
         f = open(self.fd, 'w')
         payload = ""
@@ -18,4 +26,43 @@ class editor:
             payload += (item + '\n')
         f.write(payload)
         f.close()
+    def getline(self, lineno):
+        if lineno <= len(self.source):
+            return self.source[lineno]
+        else: 
+            return 1
+    def goto(self, pos):
+        if not isint(pos):
+            return 1
+        pos = int(pos)
+        if pos <= len(self.source):
+            self.cursor = pos
+            return 0
+        else:
+            return 1
+    def isint(self, test):
+        try:
+            int(test)
+            return True
+        except ValueError:
+            return False
+    def getln(self, loc="NAN"):
+        if loc == "NAN":
+            loc = self.cursor
+        if not self.isint(loc):
+            return 1
+        loc = int(loc)
+        return self.source[loc]
+    def getpos(self):
+        return self.cursor
+    def getlen(self):
+        return len(self.source)
+    def delline(self, loc="NAN"):
+
+        if loc == "NAN":
+            loc = self.cursor
+        if not self.isint(loc):
+            return 1
+        loc = int(loc)
+        del self.source[loc]
 
--- main.py
+++ main.py
@@ -5,21 +5,39 @@ parser= argparse.ArgumentParser(description='JEDI is a Ed clone, made in the 21s
 
 # Add the arguments
 parser.add_argument('file', type=str, help='The path used to access the file to edit (Ex: ~/.bashrc')
-parser.add_argument('-p', action='store', type=str, help='The prompt string to be used')
-
+parser.add_argument('-p', nargs='?', const='', action='store', type=str, help='The prompt string to be used')
+def split(word): 
+    return [char for char in word] 
 args = parser.parse_args()
 
 e = editor(args.file)
+p = args.p
+if p == 'None':
+    p=''
 while True:
-    command = input(args.p) # TODO: Make prompt with -p
-    if command == 'a':
+    command = split(input(p))
+    if command[0] == 'a':
         data = []
         txt = ""
         while txt != "`":
+            data.append(txt) 
             txt = input()
-            data.append(txt)
+        data.remove(data[0])
         e.append(data)
-    elif command == 'w':
+    elif command[0] == 'w':
         e.write()
-    elif command == 'q':
+    elif e.isint(command[0]):
+        e.goto(command[0])
+    elif command[0] == 'p':
+        if len(command) == 1:
+           print( e.getln())
+        elif len(split(command)) == 2:
+            print(e.getln(int(command[1])))
+    elif command[0] == 'd':
+        if len(command) == 1:
+            e.delline()
+        elif len(command) == 2:
+            error = e.delline(command[1])
+    elif command[0] == 'q':
         break
+
